c16aaf4835e46c1e0b60a947de4d9a655a4c9ae4
Ed <objdog@gmail.com>
2019-01-09 22:23:59 +0000

    Final change to acronym.py, moving .upper()

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -3,5 +3,5 @@ def abbreviate(words):
     acro = []
     for i in re.findall(r"[A-Za-z\']+", words):
         if i[0].isalpha():
-            acro.append(i[0].upper())
-    return ''.join(acro)
+            acro.append(i[0])
+    return ''.join(acro).upper()
